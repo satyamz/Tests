b15dd6cf7f8ae23b4d2b7eb0c9603462dc71d2ef
Satyam Zode <satyamzode@gmail.com>
2015-03-13 15:43:06 +0530

    Test file for checking -b Option

---
+++ Test-b.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+from sys import exit
+from test.http_test import HTTPTest
+from test.base_test import HTTP, HTTPS
+from misc.wget_file import WgetFile
+import os
+
+"""
+    This test ensures that Wget can download files from HTTPS Servers in background 
+"""
+TEST_NAME = "HTTPS Downloads in background"
+############# File Definitions ###############################################
+File1 = "Would you like some Tea?"
+#File2 = "With lemon or cream?"
+#File3 = "Sure you're joking Mr. Feynman"
+
+A_File = WgetFile ("File1", File1)
+#B_File = WgetFile ("File2", File2)
+#C_File = WgetFile ("File3", File3)
+
+WGET_OPTIONS = " -b " 
+WGET_URLS = [["File1"]]
+
+Files = [[A_File]]
+#Existing_Files = [C_File]
+
+Servers = [HTTPS]
+
+ExpectedReturnCode = 0
+ExpectedDownloadedFiles = [A_File]
+
+################ Pre and Post Test Hooks #####################################
+pre_test = {
+    "ServerFiles"       : Files,
+    
+}
+test_options = {
+    "WgetCommands"      : WGET_OPTIONS,
+    "Urls"              : WGET_URLS
+}
+post_test = {
+    "ExpectedFiles"     : ExpectedDownloadedFiles,
+    "ExpectedRetcode"   : ExpectedReturnCode
+}
+
+err = HTTPTest (
+                name=TEST_NAME,
+                pre_hook=pre_test,
+                test_params=test_options,
+                post_hook=post_test,
+                protocols=Servers
+).begin ()
+
+exit (err)
+
